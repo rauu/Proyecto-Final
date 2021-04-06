a2b7f158f5484b7e1920d66531380f1a1ec189a3
rauu <raunakbinyani.binyani@gmail.com>
2021-04-06 23:57:55 +0200

    register form design in process

--- Proyecto/client/src/components/Register/Register.js
+++ Proyecto/client/src/components/Register/Register.js
@@ -1,6 +1,8 @@
 import "./Register.css";
 import logo from "../../assets/logo.png";
-import { Typography, Link, Button } from "@material-ui/core";
+import { Typography, Link, Button, Grid, TextField } from "@material-ui/core";
+import DatePicker from "@material-ui/lab/DatePicker";
+
 import LinkA from "react-router-dom/Link";
 
 const Register = () => {
@@ -25,10 +27,75 @@ const Register = () => {
         </nav>
       </Typography>
       <div className="register-first">
-        <br/>
-        <Typography variant="h2" className="white">YOUR CHANGE BEGINS HERE</Typography>
-        <Typography variant="h5" className="white">Body, Mind and Nutrition</Typography>
-        <br/>
+        <br />
+        <Typography variant="h2" className="white">
+          YOUR CHANGE BEGINS HERE
+        </Typography>
+        <Typography variant="h5" className="white">
+          Body, Mind and Nutrition
+        </Typography>
+        <br />
+      </div>
+      <div className="registerForm">
+        <br />
+        <br />
+        <br />
+        <br />
+        <form action="POST" autoComplete="off">
+          <Grid container spacing={5} className="grid-center">
+            <Grid item xs={12} sm={12} md={6} lg={6}>
+              <TextField
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} lg={6}>
+              <TextField
+                name="lastName"
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+              <TextField
+                name="username"
+                variant="outlined"
+                required
+                fullWidth
+                id="username"
+                label="UserName"
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+              <TextField
+                name="email"
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+{/*               <DatePicker
+                label="Basic example"
+                value={value}
+                onChange={(newValue) => {
+                  setValue(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              /> */}
+            </Grid>
+          </Grid>
+        </form>
       </div>
     </div>
   );
